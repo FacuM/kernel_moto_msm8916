f5edc6842fa79e4a6d8f609db4770fff09cf6975
RazorSharp <kirananto@gmail.com>
2015-11-21 11:38:01 +0530

    cpufreq: Add zzmoove governor v1.0 beta8 and Enable it
    All credits to ZaneZam !!!
    
    Changelog:
    
    Version 1.0 beta8
    
    - bump version to 1.0 beta1 because of brought forward plan 'outbreak'
    - reworked scaling logic:
      removed unessesary calls of external cpufreq function and use a static variable instead to hold the system freq table during runtime
      fixed frequency stuck on max hard and soft frequency limit (under some circumstances freq was out of scope for the main search loop)
      and added precautions to avoid problems when for what ever reason the freq table is 'messed' or even not available for the governor
      fixed not properly working scaling with descend ordered frequency table like it is for example on qualcomm platform
      added additional propotional scaling mode (mode '1' as usual decide and use the lowest freq, new mode '2' use only propotional
      frequencies like ondemand governor does - switchable as before in 'scaling_proportional' tuneable)
    - use static frequency table variable in all frequency checks agains system frequency table in the governor
    - fixed 'update_ts_time_stat idle accounting' (kernel patch for kernel version 3.0.x needed, example available in github zzmoove repository)
    - fixed non setting of scaling down threshold tuneables under some circumstances (issue on kernel 3.4 when running multible zzmoove instances)
    - changed some variable names in scaling range evaluation and debugging tuneable
    - added compatibility for kernel version 3.4 (or higher, but only tested on 3.4.0 yet)
    - added support for powersuspend (used on some platforms since kernel version 3.4)
    - added support for opo specific 'backlight ext control' (kernel patch for opo bacon devices needed, example available in github zzmoove repository)
    - added macros to exclude hotplugging functionality (default in this version is disabled=commented for opo devices because of mpdecision
      in general and unmotivated kernel crashes when using it without mpdecision) in addition disable hopplugging per default if hotplugging
      code is used to avoid accetential usage of mpdecision and buildin governor hotplugging at the same time
    
    profile header file (Version 0.3 beta1 OPO)
    
    - bump version to 0.3 beta1 because of brought forward plan 'outbreak'
    - removed dynamic freq scaling tuneable leftovers
    - added macros to switch code depending of used power management implementation
      or used supend/resume backlight hook (opo specific)
    - added macros to disable hotplugging
    - adjusted values in all profiles for opo devices (not optimized just compatible -> WIP)
    
    Signed Off : Kiran Anto <kirananto@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -456,6 +456,11 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_intelliactive)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 /*********************************************************************
